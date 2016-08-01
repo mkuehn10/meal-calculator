5e94f6599137b00b5c4d04830226f83dc828eeee
Michael Kuehn <mkuehn10@gmail.com>
2016-08-01 00:23:32 +0000

    meal calculator

---
+++ meal_calculator.js
@@ -0,0 +1,84 @@
+var Dish = function(name, cost) {
+    this.name = name;
+    this.cost = cost;
+};
+
+Dish.prototype.getDishCost = function() {
+  return this.cost;  
+};
+
+var Diner = function(name, dishes) {
+    this.name = name;
+    this.dishes = dishes;
+};
+
+Diner.prototype.getDinerName = function() {
+  return this.name;  
+};
+
+Diner.prototype.getDinerDishCosts = function() {
+  return this.dishes.map(function(dish) {
+      return dish.cost;
+  });  
+};
+
+Diner.prototype.addTotal = function() {
+    return this.getDinerDishCosts().reduce(function(previousValue, currentValue) {
+       return previousValue + currentValue; 
+    });
+};
+
+Diner.prototype.calculateTax = function(taxRate) {
+    return this.addTotal() * taxRate;
+};
+
+Diner.prototype.calculateTip = function(tipRate) {
+    return this.addTotal() * tipRate;
+};
+
+Diner.prototype.calculateTotal = function(taxRate, tipRate) {
+    return this.addTotal() + this.calculateTax(taxRate) + this.calculateTip(tipRate);
+};
+
+var Bill = function(diners, taxRate, tipRate) {
+  this.diners = diners; 
+  this.taxRate = taxRate;
+  this.tipRate = tipRate;
+};
+
+Bill.prototype.printTotal = function() {
+    var taxRate = this.taxRate;
+    var tipRate = this.tipRate;
+    this.diners.forEach(function(diner) {
+        console.log(diner.getDinerName() + ': Total ($' + diner.calculateTotal(taxRate, tipRate).toFixed(2) + '), Tax ($' + diner.calculateTax(taxRate).toFixed(2) + '), Tip ($' + diner.calculateTip(tipRate).toFixed(2) + ')');
+    });
+};
+
+Bill.prototype.printTips = function() {
+    var tipRate = this.tipRate;
+    this.diners.forEach(function(diner) {
+        console.log(diner.getDinerName() + ' needs to tip $' + diner.calculateTip(tipRate).toFixed(2));
+    });
+};
+
+Bill.prototype.breakdown = function() {
+    var taxRate = this.taxRate;
+    var tipRate = this.tipRate;
+    this.diners.forEach(function(diner) {
+        console.log(diner.getDinerName() + ' owes $' + diner.calculateTotal(taxRate, tipRate).toFixed(2));
+    });
+};
+
+var dishOne = new Dish("Moo Goo Gai Pan", 12.75);
+var dishTwo = new Dish("French Fries", 4.75);
+var dishThree= new Dish("Steak", 20.50);
+var dishFour = new Dish("Mashed Potatoes", 3.50);
+
+var firstDiner = new Diner("Jack", [dishOne, dishTwo]);
+var secondDiner = new Diner("Diane", [dishThree, dishFour]);
+
+var myBill = new Bill([firstDiner, secondDiner], 0.07, 0.20);
+
+myBill.breakdown();
+myBill.printTips(0.20);
+myBill.printTotal();
